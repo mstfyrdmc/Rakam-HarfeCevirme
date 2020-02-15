927d2257563f58dc6bacc5da6b489932d98add64
mstfyrdmc <45285832+AdamGiby@users.noreply.github.com>
2020-02-15 19:14:17 +0300

    RakamiHarfeCevirme

---
+++ WFARakam_harfe Cevirme/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace WFARakam_harfe_Cevirme
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtsayi = new System.Windows.Forms.TextBox();
+            this.lblsonuc = new System.Windows.Forms.Label();
+            this.btnCevir = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtsayi
+            // 
+            this.txtsayi.Location = new System.Drawing.Point(27, 27);
+            this.txtsayi.Name = "txtsayi";
+            this.txtsayi.Size = new System.Drawing.Size(100, 20);
+            this.txtsayi.TabIndex = 0;
+            // 
+            // lblsonuc
+            // 
+            this.lblsonuc.AutoSize = true;
+            this.lblsonuc.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblsonuc.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblsonuc.ForeColor = System.Drawing.Color.DarkRed;
+            this.lblsonuc.Location = new System.Drawing.Point(27, 56);
+            this.lblsonuc.Name = "lblsonuc";
+            this.lblsonuc.Size = new System.Drawing.Size(2, 20);
+            this.lblsonuc.TabIndex = 1;
+            // 
+            // btnCevir
+            // 
+            this.btnCevir.Location = new System.Drawing.Point(133, 25);
+            this.btnCevir.Name = "btnCevir";
+            this.btnCevir.Size = new System.Drawing.Size(90, 23);
+            this.btnCevir.TabIndex = 2;
+            this.btnCevir.Text = "Cevir";
+            this.btnCevir.UseVisualStyleBackColor = true;
+            this.btnCevir.Click += new System.EventHandler(this.btnCevir_Click);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnCevir;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(287, 85);
+            this.Controls.Add(this.btnCevir);
+            this.Controls.Add(this.lblsonuc);
+            this.Controls.Add(this.txtsayi);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtsayi;
+        private System.Windows.Forms.Label lblsonuc;
+        private System.Windows.Forms.Button btnCevir;
+    }
+}
+
---
+++ WFARakam_harfe Cevirme/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WFARakam_harfe_Cevirme
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCevir_Click(object sender, EventArgs e)
+        {
+            int sayi = Convert.ToInt32(txtsayi.Text);
+
+            string[] birler = { "", "bir", "iki", "üç", "dört", "beş", "alti", "yedi", "sekiz", "dokuz" };
+            string[] onlar = { "", "on", "yirmi", "otuz", "kirk", "elli", "altmiş", "yetmiş", "seksen", "doksan" };
+            string[] yüzler = { "", "yüz", "ikiyüz", "üçyüz", "dörtyüz", "beşyüz", "altiyüz", "yediyüz", "sekizyüz", "dokuzyüz" };
+
+            int a = sayi / 1000;
+            int b = (sayi%1000/ 100);
+            int c = (sayi % 100) / 10;
+            int d = sayi % 10;
+
+            if (sayi == 0)
+                lblsonuc.Text = "Sifir";
+
+            if (a > 0)
+            {
+                if (a != 1)
+                    lblsonuc.Text=birler[a] + " ";
+
+                lblsonuc.Text = lblsonuc.Text+" bin ";
+            }
+
+            if (b > 0)
+               lblsonuc.Text=lblsonuc.Text+yüzler[b] + " ";
+
+            if (c > 0)
+                lblsonuc.Text=lblsonuc.Text+onlar[c] + " ";
+
+            if (d > 0)
+                lblsonuc.Text = lblsonuc.Text+birler[d] + " ";
+        }
+    }
+}
